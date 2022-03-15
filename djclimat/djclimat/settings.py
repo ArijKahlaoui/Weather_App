38e4e42003726df84e3be3f01c6dbe0c30fa1465
Arij_Kahlaoui <82058469+arij-kh@users.noreply.github.com>
2022-03-15 18:01:42 +0100

    secret key deleted

--- djclimat/djclimat/settings.py
+++ djclimat/djclimat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hk4l&qnz3nmuhs+^(oru71q@b=fdx9(j%89$yc)p#ypoq(wkpn'
+SECRET_KEY = *******************************************************
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
